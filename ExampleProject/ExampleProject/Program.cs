1db487b85c6e79bac8679d0fe7c8462604048f13
Güven <sezginguvensari@gmail.com>
2021-12-10 17:26:47 +0100

    We learn the veriables

--- ExampleProject/ExampleProject/Program.cs
+++ ExampleProject/ExampleProject/Program.cs
@@ -6,7 +6,30 @@ namespace ExampleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Example Project!");
+           Console.WriteLine("Hello Example Project!");
+
+
+            #region Veriables
+
+            int x = 5;
+            int y = 3;
+            float z = 4.7f;
+            float num = 10.6f;
+            string String1 = "First";
+            string String2 = "Second";
+            char Char1 = 'a';
+            char Char2 = 'b';
+            bool Complete = false;
+            bool incomplete = true;
+
+
+
+
+
+
+            #endregion
+
+
         }
     }
 }
