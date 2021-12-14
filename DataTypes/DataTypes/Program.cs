74f015e504470abd1783ea6ec2e5ccf2bc17e989
Güven <sezginguvensari@gmail.com>
2021-12-14 19:56:31 +0100

    Basic Data Types

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region BasicDataTypes
+
+            int example = 1;  // System.Int32 example = 1;
+
+            Type sType = typeof(string);
+            Console.WriteLine(sType.FullName);
+
+
+
+
+            #endregion
+            
+        }
+    }
+
+
+
+
+
+
+
+}
