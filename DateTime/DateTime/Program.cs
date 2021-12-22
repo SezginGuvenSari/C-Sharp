e9dbeb73bdbcc861c3636ce9f44bcf8865e35e66
Güven <sezginguvensari@gmail.com>
2021-12-22 01:00:28 +0100

    DateTime and TimeSpan

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DateTimes
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+
+            #region DateTime
+
+            System.Console.WriteLine(DateTime.Now);
+
+            DateTime date = new DateTime(2020,1,9);
+
+            System.Console.WriteLine(date);
+
+
+            #endregion
+
+            #region TimeSpan
+
+            TimeSpan timeSpan = new TimeSpan(1,2,0);
+            System.Console.WriteLine(timeSpan.Hours);
+            System.Console.WriteLine(timeSpan.Minutes);
+            System.Console.WriteLine(timeSpan.Seconds);
+
+            #endregion
+
+
+
+        }
+    }
+}
