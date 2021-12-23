47552382b652970b0e7ce76a6eb16f2470f29742
Güven <sezginguvensari@gmail.com>
2021-12-23 01:12:52 +0100

    If , else if , else

---
+++ ControlFlowStatements/ControlFlowStatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ControlFlowStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region IF,else if,else
+
+            /* if (3 < 5)
+             {
+                 System.Console.WriteLine("3 is less than 5");
+             }*/
+            if (5 < 3)
+            {
+                System.Console.WriteLine("3 is less than 5");
+            }
+            
+            else if (2 < 4)
+            
+                System.Console.WriteLine("2 is less than 4");
+            }
+
+
+            #endregion
+        }
+    }
+
