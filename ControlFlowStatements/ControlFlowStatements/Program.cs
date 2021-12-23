e0a9e0f4e1faace43ca8eff13ea89de2c6f7459d
Güven <sezginguvensari@gmail.com>
2021-12-23 01:25:38 +0100

    Switch Statement

--- ControlFlowStatements/ControlFlowStatements/Program.cs
+++ ControlFlowStatements/ControlFlowStatements/Program.cs
@@ -16,14 +16,51 @@ namespace ControlFlowStatements
             {
                 System.Console.WriteLine("3 is less than 5");
             }
-            
+
             else if (2 < 4)
-            
+
                 System.Console.WriteLine("2 is less than 4");
+
+
+
+            #endregion
+
+
+            #region SwitchStatement
+
+            int value = 11;
+
+            switch (value)
+            {
+                case 1:
+                    System.Console.WriteLine("1");
+                    break;
+                case 2:
+                    System.Console.WriteLine("2");
+                    break;
+                case 3:
+                    System.Console.WriteLine("3");
+                    break;
+                case 4:
+                    System.Console.WriteLine("4");
+                    break;
+                case 5:
+                    System.Console.WriteLine("5");
+                    break;
+                case 6:
+                    System.Console.WriteLine("6");
+                    break;
+
+                default:
+                    System.Console.WriteLine("Other");
+                    break;
+
             }
+
 
 
             #endregion
         }
-    }
+}
+}
 
