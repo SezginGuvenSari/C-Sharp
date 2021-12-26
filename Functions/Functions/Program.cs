627b730508ae9a29675d45bdd63b44b8f41d24ac
Güven <sezginguvensari@gmail.com>
2021-12-26 21:24:45 +0100

    Properties and Anonymus Functions ,  Lambda Expressions

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -8,7 +8,7 @@ namespace Functions
 
         #region Constructors
 
-        int programNumber;
+      /*  int programNumber;
         string exampleName;
 
         public  Program(int num)
@@ -29,17 +29,54 @@ namespace Functions
         }
 
 
-
+        */
         #endregion
 
+
+        #region Properties
+        /*
+
+        // public string Id { get; set; } 
+        private string id;
+
+
+        public string Id
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = value;
+            }
+        }
+        */
+        #endregion
 
 
         static void Main(string[] args)
         {
+            /* Program program = new Program();
+             program.Id = "2312312";
+             System.Console.WriteLine(program.Id);
+             Program pr = new Program(5);
+             Program ps = new Program(5, "Guven");
+             System.Console.WriteLine(pr.programNumber);*/
 
-            Program pr = new Program(5);
-            Program ps = new Program(5, "Guven");
-            System.Console.WriteLine(pr.programNumber);
+
+            #region Anonymus Functions and  Lambda Expressions
+
+            Action debug = () => System.Console.WriteLine("Hello World");
+            debug();
+
+            Action<string> debug2 = (s) => System.Console.WriteLine(s);
+            debug2("Example");
+
+
+            #endregion
+
+
 
 
 
