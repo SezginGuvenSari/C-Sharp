57a9eed1529c8cc5bafcd0d41020ebd9e77170f0
Güven <sezginguvensari@gmail.com>
2021-12-26 20:50:44 +0100

    Constructors

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Functions
+{
+    public class Program
+    {
+
+
+        #region Constructors
+
+        int programNumber;
+        string exampleName;
+
+        public  Program(int num)
+
+            {
+
+            programNumber = num;
+
+            }
+
+        public Program(int num,string name)
+
+        {
+
+            programNumber = num;
+            exampleName = name;
+
+        }
+
+
+
+        #endregion
+
+
+
+        static void Main(string[] args)
+        {
+
+            Program pr = new Program(5);
+            Program ps = new Program(5, "Guven");
+            System.Console.WriteLine(pr.programNumber);
+
+
+
+        }
+    }
+}
