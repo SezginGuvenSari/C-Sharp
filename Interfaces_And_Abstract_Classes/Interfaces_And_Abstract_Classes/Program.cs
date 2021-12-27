02691ef545e2a4d023a5dd094adba911492fba00
Güven <sezginguvensari@gmail.com>
2021-12-27 15:54:01 +0100

    Implementing an Interface

--- Interfaces_And_Abstract_Classes/Interfaces_And_Abstract_Classes/Program.cs
+++ Interfaces_And_Abstract_Classes/Interfaces_And_Abstract_Classes/Program.cs
@@ -2,11 +2,63 @@
 
 namespace Interfaces_And_Abstract_Classes
 {
-    class Program
+    class Program : IExample,ISecondExample
     {
+
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            
+
+
         }
+        #region Implementing an Interface
+
+        public void Example()
+        {
+            System.Console.WriteLine("Hello World");
+        }
+
+        public int Example2()
+        {
+            return 1;
+        }
+
+        public string Example3(string string1, string string2)
+        {
+            return string1 + " " + string2;
+        }
+
+
+        #endregion
+
     }
+
+
+
+    #region  Creating an Interface 
+
+
+    interface IExample
+    {
+
+        void Example();
+        int Example2();
+        string Example3(string string1, string string2);
+
+
+    }
+
+    interface ISecondExample
+    {
+
+    }
+
+
+    #endregion
 }
