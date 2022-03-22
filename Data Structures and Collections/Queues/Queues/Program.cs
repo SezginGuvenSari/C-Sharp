183b25b2f184c1aeced6f7bd6a085bbe51565204
Güven Sarı <59062942+SezginGuvenSari@users.noreply.github.com>
2022-03-22 13:42:06 +0300

    Queues

---
+++ Data Structures and Collections/Queues/Queues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            queue.Enqueue("a");
+            queue.Enqueue("b");
+            queue.Enqueue("c");
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Peek());
+        }
+    }
+}
